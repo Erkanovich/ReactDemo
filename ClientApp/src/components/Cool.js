b4d7482c802fa00c0626d5e428eb9b8c1ee86bcb
Erkanovich <erinik90@gmail.com>
2021-05-07 10:03:40 +0200

    initial commit

---
+++ ClientApp/src/components/Cool.js
@@ -0,0 +1,23 @@
+﻿import React, { Component } from 'react';
+import { Box } from './Box';
+
+export class Cool extends Component {
+  constructor(props) {
+    console.log(props);
+    super(props);
+  }
+
+  render() {
+    let allMyBoxes = [];
+    for (let i = 0; i < 100; i++) {
+      allMyBoxes.push(<Box key={i}></Box>);
+    }
+    console.log(allMyBoxes.length);
+    return (
+      <div>
+        <h1>This is a cool page</h1>
+        {allMyBoxes}
+      </div>
+      );
+  }
+}
