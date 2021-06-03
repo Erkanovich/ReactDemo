2136ad0e5616ef888e286111b3e060c6cb0b9572
Erkanovich <erinik90@gmail.com>
2021-06-03 10:52:23 +0200

    Updates

---
+++ precache-manifest.c992ed777f2636c2f1c7d45abd52f788.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac8dcff07c2b26d6eaa31ec269e6ae3a",
+    "url": "/ReactDemo/index.html"
+  },
+  {
+    "revision": "c2d01a3638570f5029db",
+    "url": "/ReactDemo/static/css/2.56e33495.chunk.css"
+  },
+  {
+    "revision": "e9d98c764f97e7fa7d42",
+    "url": "/ReactDemo/static/css/main.13b298a8.chunk.css"
+  },
+  {
+    "revision": "c2d01a3638570f5029db",
+    "url": "/ReactDemo/static/js/2.4f2bcfd6.chunk.js"
+  },
+  {
+    "revision": "83e9426a94f2d019bb00e89f0b2c5263",
+    "url": "/ReactDemo/static/js/2.4f2bcfd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9d98c764f97e7fa7d42",
+    "url": "/ReactDemo/static/js/main.f6708018.chunk.js"
+  },
+  {
+    "revision": "053ed38042503ab6d9e4",
+    "url": "/ReactDemo/static/js/runtime-main.d2e90180.js"
+  }
+]);
